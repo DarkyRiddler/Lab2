10c97a54a34660f761f6fd8da5c1cd110012b9c2
DarkyRiddler <ppeterpplay@gmail.com>
2025-10-21 08:53:03 +0200

    Com2_1.1

---
+++ script.js
@@ -0,0 +1,40 @@
+(function () {
+  const example = document.getElementById('example')
+  const cw1 = document.getElementById('cw1')
+  const cw2 = document.getElementById('cw2')
+  const cw3 = document.getElementById('cw3')
+  const answer = document.getElementById('answer')
+
+  example.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        console.log(array)
+        answer.innerHTML = JSON.stringify(array);
+      })
+  })
+
+  cw1.addEventListener("click", function () {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(array => {
+        const titles = array.map(item => item.title);
+        let htmlContent = '<ul class="posts-list">';
+        titles.forEach(title => {
+          htmlContent += `<li>${title}</li>`;
+        });
+        htmlContent += '</ul>';
+        
+        answer.innerHTML = htmlContent;
+      })
+  })
+
+  cw2.addEventListener("click", function () {
+    //TODO
+  })
+
+  cw3.addEventListener("click", function () {
+    //TODO
+  })
+
+})();
